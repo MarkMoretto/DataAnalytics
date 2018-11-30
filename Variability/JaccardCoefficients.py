b72709aa76dd2d5c1d2f3084bc1eb008210930b2
Mark M <otteromkram@gmail.com>
2018-11-29 21:57:00 -0500

    Module for text mining classes and functions.

--- Variability/JaccardCoefficients.py
+++ Variability/JaccardCoefficients.py
@@ -1,5 +1,50 @@
 #! python3
 
+from __future__ import print_function
+from collections import OrderedDict
+
+class FormatDF(object):
+
+    FLOAT_LIST = ['float', 'float32', 'float64',]
+    INT_LIST = ['int', 'int32', 'int64',]
+
+
+    def __init__(self, data_frame):
+        self.df = data_frame
+        self.__type_dict(self.df)
+
+
+    def __str__(self):
+        return f'Class: \'<{self.__class__.__name__}>\''
+
+
+    def __type_dict(self, df = None):
+        self.type_dic = OrderedDict()
+        for i in df:
+            self.type_dic[i] = df[i].dtype
+        self.__find_numeric()
+
+
+    def __find_numeric(self):
+        full_list = self.FLOAT_LIST + self.INT_LIST
+        self.numeric_dic = {i:self.type_dic[i] for i in self.type_dic.keys() if self.type_dic[i] in full_list}
+
+
+    def clean_data(data_frame, print_nan_count=False):
+        """ Will convert int to float and fill nan with zero. """
+        self.numeric_dic = self.__find_numeric()
+        df = data_frame.copy(deep = True)
+        if print_nan_count:
+            print(f'Null values found in each column:\n{df.isna().sum()}')
+        else:
+            df = df_1.fillna(0)
+            for i in df.select_dtypes(include=['int64']):
+                df[i] = df[i].astype(float)
+            return df
+
+
+
+
 class Jaccard(object):
     """
     Find Jaccard coefficients, return results to dataframe.
